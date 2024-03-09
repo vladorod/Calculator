e8a34e561744a3ba47b1f7458b002c5aae9f2e2e
Bigger134 <w1541w@yandex.ru>
2024-03-09 13:38:46 +0300

    refactor: добавлено required

--- src/utils/code.ts
+++ src/utils/code.ts
@@ -1,23 +1,26 @@
-import { sendInfo, getInfo } from "./functions.ts";
-import IMask from "imask";
+import { sendInfo } from "./functions.ts";
+import IMask, { FactoryArg, MaskedOptions } from "imask";
 
 document.addEventListener("DOMContentLoaded", async function () {
   const nameField = document.querySelector("#name") as Element;
   const mailField = document.querySelector("#mail") as Element;
   const ideaField = document.querySelector("#idea") as Element;
-  const buttonSend = document.querySelector("#send") as Element;
-  const element = document.querySelector("#phone") as Element;
+  // const buttonSend = document.querySelector("#sendForm") as Element;
+  const element = document.querySelector("#phone") as HTMLInputElement;
+  const contactForm = document.querySelector("#formContact") as Element;
 
   // маска для телефонного номера
-  const maskOptions = {
+  const maskOptions: FactoryArg = {
     mask: "+{7}(000)000-00-00",
     lazy: false,
   };
-
   const mask = IMask(element, maskOptions);
-
+  element.value = "";
+  mask.updateValue();
+  console.log(contactForm);
   // реакция сайта на нажатие кнопки "отправить"
-  buttonSend.addEventListener("click", async () => {
+  contactForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
     await sendInfo(
       nameField.value,
       mask.value,
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-export const sendInfo = async (
+export const sendInfo = (
   name: string,
   phone: string,
   mail: string,
   idea: string
 ) => {
-  const url = "https://willbehome.ru/callback";
+  const url = "/wp-json/dabudetdom/v1/application";
   axios.post(url, {
     name,
     phone,
