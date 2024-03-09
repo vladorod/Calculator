d278807016ee3ec4c8f65ecfa0661fa6614a346e
Vladislav <34486493+vladorod@users.noreply.github.com>
2024-03-09 14:26:40 +0300

    feat: Добавлена валидация на phone

--- src/utils/code.ts
+++ src/utils/code.ts
@@ -2,11 +2,11 @@ import { sendInfo } from "./functions.ts";
 import IMask, { FactoryArg, MaskedOptions } from "imask";
 
 document.addEventListener("DOMContentLoaded", async function () {
-  const nameField = document.querySelector("#name") as Element;
-  const mailField = document.querySelector("#mail") as Element;
-  const ideaField = document.querySelector("#idea") as Element;
+  const nameField = document.querySelector("#name") as HTMLInputElement;
+  const mailField = document.querySelector("#mail") as HTMLInputElement;
+  const ideaField = document.querySelector("#idea") as HTMLInputElement;
   // const buttonSend = document.querySelector("#sendForm") as Element;
-  const element = document.querySelector("#phone") as HTMLInputElement;
+  const phoneField = document.querySelector("#phone") as HTMLInputElement;
   const contactForm = document.querySelector("#formContact") as Element;
 
   // маска для телефонного номера
@@ -14,18 +14,19 @@ document.addEventListener("DOMContentLoaded", async function () {
     mask: "+{7}(000)000-00-00",
     lazy: false,
   };
-  const mask = IMask(element, maskOptions);
-  element.value = "";
+  const mask = IMask(phoneField, maskOptions);
+  phoneField.value = "";
   mask.updateValue();
-  console.log(contactForm);
+
   // реакция сайта на нажатие кнопки "отправить"
   contactForm.addEventListener("submit", async (event) => {
     event.preventDefault();
-    await sendInfo(
-      nameField.value,
-      mask.value,
-      mailField.value,
-      ideaField.value
-    );
+    if (phoneField.value.match(/_/ig)) {
+      phoneField.setCustomValidity("Некоректный номер");
+      phoneField.reportValidity();
+    } else {
+      phoneField.setCustomValidity("");
+      sendInfo(nameField.value, phoneField.value, mailField.value, ideaField.value)
+    }
   });
 });
