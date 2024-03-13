03911a60afa9c4727251c534d1622970a2ce3eaa
Bigger134 <w1541w@yandex.ru>
2024-03-13 14:46:57 +0300

    refactor: подключение zustand

--- src/utils/code.ts
+++ src/utils/code.ts
@@ -1,11 +1,30 @@
+import create from "zustand";
+
+const store = create(set => ({
+    roof: { isMenuOpen: false, currentValue: null },
+    // square:'',
+    // sceleton:'',
+    openRoof: (value: boolean) => {
+        set(state => ({ roof: { ...state.roof, isMenuOpen: value, currentValue: 'Hi!' } }))
+    }
+}))
+store.subscribe((state) => {
+    console.log(state);
+}, (state) => state.roof)
+
 document.addEventListener('DOMContentLoaded', () => {
+    const data = document.querySelector('input[name="radio_menu_sqare"]:checked')
+    console.log(data)
+
+
+    //Код для выпадающего меню
     const pluses = Array.from(document.querySelectorAll('.plus_image'))
-    console.log(pluses);
     pluses.forEach(e => {
         e.addEventListener('click', e => {
+            const openRoof = store.getState().openRoof
+            openRoof(false);
             if (e) {
                 const menu = e.target.dataset.menu;
-                console.log(menu);
                 document.querySelectorAll('.dropdown_menu').forEach(e => {
                     const menu_element = document.querySelector(`[data-target=${menu}]`);
                     const button_element = document.querySelector(`[data-menu=${menu}]`);
