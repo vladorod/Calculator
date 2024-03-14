7d9c9dde6e003d5862a1ce0af5ad3c5c1d1feabe
Bigger134 <w1541w@yandex.ru>
2024-03-14 15:55:28 +0300

    refactor: Перенос данных в форму и суммирование

--- src/utils/code.ts
+++ src/utils/code.ts
@@ -2,20 +2,123 @@ import create from "zustand";
 
 const store = create(set => ({
     roof: { isMenuOpen: false, currentValue: null },
-    // square:'',
-    // sceleton:'',
-    openRoof: (value: boolean) => {
-        set(state => ({ roof: { ...state.roof, isMenuOpen: value, currentValue: 'Hi!' } }))
+    square: { isMenuOpen: false, currentValue: null },
+    sceleton: { isMenuOpen: false, currentValue: null },
+    foundation: { isMenuOpen: false, currentValue: null },
+    log: { isMenuOpen: false, currentValue: null },
+    openRoof: (value: boolean, radio: string) => {
+        set(state => ({ roof: { ...state.roof, isMenuOpen: value, currentValue: radio } }))
     }
 }))
 store.subscribe((state) => {
     console.log(state);
 }, (state) => state.roof)
 
-document.addEventListener('DOMContentLoaded', () => {
-    const data = document.querySelector('input[name="radio_menu_sqare"]:checked')
-    console.log(data)
 
+document.addEventListener('DOMContentLoaded', () => {
+
+    const radios = Array.from(document.querySelectorAll('.dropdown_menu'))
+    let valueArr = new Array(6);
+    valueArr[5] = 2;
+    radios.forEach(radio => {
+        radio.addEventListener('click', e => {
+            if (e) {
+                if (document.querySelector('input[name="radio_menu_sqare"]:checked')) {
+                    const data = document.querySelector('input[name="radio_menu_sqare"]:checked').id
+                    const text = document.querySelector(`label[for="${data}"]`).innerText
+                    const value = Number(document.querySelector(`input[id="${data}"]`).value)
+                    const element = document.getElementById('sqare_result');
+                    valueArr[0] = value;
+                    if (element) {
+                        element.textContent = text;
+                    } else {
+                        console.error('Element with id "sqare_result" not found');
+                    }
+                }
+            }
+            if (e) {
+                if (document.querySelector('input[name="radio_menu_roof"]:checked')) {
+                    const data = document.querySelector('input[name="radio_menu_roof"]:checked').id
+                    const text = document.querySelector(`label[for="${data}"]`).innerText
+                    const value = Number(document.querySelector(`input[id="${data}"]`).value)
+                    const element = document.getElementById('roof_result');
+                    valueArr[1] = value;
+                    if (element) {
+                        element.textContent = text;
+                    } else {
+                        console.error('Element with id "roof_result" not found');
+                    }
+                }
+            }
+            if (e) {
+                if (document.querySelector('input[name="radio_menu_sceleton"]:checked')) {
+                    const data = document.querySelector('input[name="radio_menu_sceleton"]:checked').id
+                    const text = document.querySelector(`label[for="${data}"]`).innerText
+                    const value = Number(document.querySelector(`input[id="${data}"]`).value)
+                    const element = document.getElementById('sceleton_result');
+                    valueArr[2] = value;
+                    if (element) {
+                        element.textContent = text;
+                    } else {
+                        console.error('Element with id "sceleton_result" not found');
+                    }
+                }
+            }
+            if (e) {
+                if (document.querySelector('input[name="radio_menu_foundation"]:checked')) {
+                    const data = document.querySelector('input[name="radio_menu_foundation"]:checked').id
+                    const text = document.querySelector(`label[for="${data}"]`).innerText
+                    const value = Number(document.querySelector(`input[id="${data}"]`).value)
+                    const element = document.getElementById('foundation_result');
+                    valueArr[3] = value;
+                    if (element) {
+                        element.textContent = text;
+                    } else {
+                        console.error('Element with id "foundation_result" not found');
+                    }
+                }
+            }
+            if (e) {
+                if (document.querySelector('input[name="radio_menu_log"]:checked')) {
+                    const data = document.querySelector('input[name="radio_menu_log"]:checked').id
+                    const text = document.querySelector(`label[for="${data}"]`).innerText
+                    const value = Number(document.querySelector(`input[id="${data}"]`).value)
+                    const element = document.getElementById('log_result');
+                    valueArr[4] = value;
+                    if (element) {
+                        element.textContent = text;
+                    } else {
+                        console.error('Element with id "log_result" not found');
+                    }
+                }
+            }
+            let summ = 0;
+            valueArr.map((item) => summ += item);
+            console.log(summ);
+            document.getElementById('summ').textContent = summ
+        })
+    })
+
+    const builds = Array.from(document.querySelectorAll('.calc_form_radio_btn'))
+    builds.forEach(build => {
+        build.addEventListener('click', e => {
+            if (e) {
+                if (document.querySelector('input[name="radio_btn"]:checked')) {
+                    const data = document.querySelector('input[name="radio_btn"]:checked').id
+                    const value = Number(document.querySelector(`input[id="${data}"]`).value)
+                    valueArr[5] = value;
+                }
+            }
+            let summ = 0;
+            valueArr.map((item) => summ += item);
+            console.log(summ);
+            document.getElementById('summ').textContent = summ
+        })
+    })
+    let summ = 0;
+    valueArr.map((item) => summ += item);
+    console.log(summ);
+    document.getElementById('summ').textContent = summ
 
     //Код для выпадающего меню
     const pluses = Array.from(document.querySelectorAll('.plus_image'))
